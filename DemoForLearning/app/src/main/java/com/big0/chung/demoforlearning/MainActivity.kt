909c5ecba82d73cd05728ca5d4cc13bb2940b376
ChungTV <tranchung927@gmail.com>
2018-06-12 15:52:42 +0700

    T01.03-AddScrolling

--- DemoForLearning/app/src/main/java/com/big0/chung/demoforlearning/MainActivity.kt
+++ DemoForLearning/app/src/main/java/com/big0/chung/demoforlearning/MainActivity.kt
@@ -9,8 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-    // COMPLETED (2) Loop through each toy and append the name to the TextView (add \n for spacing)
         /*
          * Iterate through the array and append the Strings to the TextView. The reason why we add
          * the "\n\n\n" after the String is to give visual separation between each String in the
