e7543cca2b71af0266fda9313dbd3b99d32e1714
ChungTV <tranchung927@gmail.com>
2018-06-12 16:14:23 +0700

    T02.02-AddMenu

--- DemoForLearning/app/src/main/java/com/big0/chung/demoforlearning/MainActivity.kt
+++ DemoForLearning/app/src/main/java/com/big0/chung/demoforlearning/MainActivity.kt
@@ -2,31 +2,46 @@ package com.big0.chung.demoforlearning
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    // COMPLETED (26) Create an EditText variable called mSearchBoxEditText
     lateinit var mSearchBoxEditText: EditText
-
-    // COMPLETED (27) Create a TextView variable called mUrlDisplayTextView
     lateinit var mUrlDisplayTextView: TextView
-
-    // COMPLETED (28) Create a TextView variable called mSearchResultsTextView
     lateinit var mSearchResultsTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // COMPLETED (29) Use findViewById to get a reference to mSearchBoxEditText
         mSearchBoxEditText = findViewById(R.id.et_search_box)
-
-        // COMPLETED (30) Use findViewById to get a reference to mUrlDisplayTextView
         mUrlDisplayTextView = findViewById(R.id.tv_url_display)
-        
-        // COMPLETED (31) Use findViewById to get a reference to mSearchResultsTextView
         mSearchResultsTextView = findViewById(R.id.tv_github_search_results_json)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // COMPLETED (9) Within onCreateOptionsMenu, use getMenuInflater().inflate to inflate the menu
+        menuInflater.inflate(R.menu.main, menu)
+        // COMPLETED (10) Return true to display your menu
+        return true
+    }
+
+    // COMPLETED (11) Override onOptionsItemSelected
+    // COMPLETED (12) Within onOptionsItemSelected, get the ID of the item that was selected
+    // COMPLETED (13) If the item's ID is R.id.action_search, show a Toast and return true to tell Android that you've handled this menu click
+    // COMPLETED (14) Don't forgot to call .show() on your Toast
+    // COMPLETED (15) If you do NOT handle the menu click, return super.onOptionsItemSelected to let Android handle the menu click
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_search) {
+
+            Toast.makeText(this, "Search clicked", Toast.LENGTH_SHORT).show()
+
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
