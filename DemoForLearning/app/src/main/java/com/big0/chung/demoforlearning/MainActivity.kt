5e2e75f7ae044c3e9b9e999657e231e7f19a0619
ChungTV <tranchung927@gmail.com>
2018-06-13 11:31:40 +0700

    T03.06-Solution-RefreshMenuButton

--- DemoForLearning/app/src/main/java/com/big0/chung/demoforlearning/MainActivity.kt
+++ DemoForLearning/app/src/main/java/com/big0/chung/demoforlearning/MainActivity.kt
@@ -3,6 +3,9 @@ package com.big0.chung.demoforlearning
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -40,5 +43,33 @@ class MainActivity : AppCompatActivity() {
          * The GreenAdapter is responsible for displaying each item in the list.
          */
         rv_numbers.adapter = mAdapter
+    }
+
+
+    // COMPLETED (7) Override onCreateOptionsMenu
+    // COMPLETED (8) Use getMenuInflater().inflate to inflate the menu
+    // COMPLETED (9) Return true to display this menu
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    // COMPLETED (10) Override onOptionsItemSelected
+    // COMPLETED (11) Within this method, get the ID from the MenuItem
+    // COMPLETED (12) If the ID equals R.id.action_refresh, create and set a new adapter on the RecyclerView and return true
+    // COMPLETED (13) For now, for all other IDs, return super.onOptionsItemSelected
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        /*
+            * When you click the reset menu item, we want to start all over
+            * and display the pretty gradient again. There are a few similar
+            * ways of doing this, with this one being the simplest of those
+            * ways. (in our humble opinion)
+            */
+        if (item.itemId == R.id.action_refresh) {
+            mAdapter.viewHolderCount = 0
+            rv_numbers.adapter = mAdapter
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
