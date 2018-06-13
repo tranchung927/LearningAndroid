8b948636873a3fc34970ee7c95871c5c750cd761
ChungTV <tranchung927@gmail.com>
2018-06-13 16:21:30 +0700

    T04a.03-PassingDataBetweenActivities

--- DemoForLearning/app/src/main/java/com/big0/chung/demoforlearning/ChildActivity.kt
+++ DemoForLearning/app/src/main/java/com/big0/chung/demoforlearning/ChildActivity.kt
@@ -1,13 +1,37 @@
 package com.big0.chung.demoforlearning
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_child.*
 
-// COMPLETED (1) Use Android Studio's Activity wizard to create a new Activity called ChildActivity
 class ChildActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_child)
+        // COMPLETED (3) Use the getIntent method to store the Intent that started this Activity in a variable
+        /*
+         * Here is where all the magic happens. The getIntent method will give us the Intent that
+         * started this particular Activity.
+         */
+        if (intent.hasExtra(Intent.EXTRA_TEXT)) {
+            // COMPLETED (4) If the Intent contains the correct extra, retrieve the text
+            /*
+             * Now that we've checked to make sure the extra we are looking for is contained within
+             * the Intent, we can extract the extra. To do that, we simply call the getStringExtra
+             * method on the Intent. There are various other get*Extra methods you can call for
+             * different types of data. Please feel free to explore those yourself.
+             */
+            val textEntered = intent.getStringExtra(Intent.EXTRA_TEXT)
+
+            // COMPLETED (5) If the Intent contains the correct extra, use it to set the TextView text
+            /*
+             * Finally, we can set the text of our TextView (using setText) to the text that was
+             * passed to this Activity.
+             */
+            tv_display.text = textEntered
+        }
+
     }
 }
--- DemoForLearning/app/src/main/java/com/big0/chung/demoforlearning/MainActivity.kt
+++ DemoForLearning/app/src/main/java/com/big0/chung/demoforlearning/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,17 +14,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onclickStartNewActivity(view: View) {
-        // COMPLETED (1) Store ChildActivity.class in a Class object called destinationActivity
-        /* This is the class that we want to start (and open) when the button is clicked. */
+
+        // COMPLETED (1) Retrieve the text from the EditText and store it in a variable
+        /* We'll first get the text entered by the user in the EditText */
+
+        val textEntered = et_text_entry.text.toString()
+
         val destinationActivity = ChildActivity::class.java
 
-        // COMPLETED (2) Create an Intent to start ChildActivity
-        /*
-         * Here, we create the Intent that will start the Activity we specified above in
-         * the destinationActivity variable. The constructor for an Intent also requires a
-         * context, which we stored in the variable named "context".
-         */
         val startChildActivityIntent = Intent(this, destinationActivity)
+
+        // COMPLETED (2) Use the putExtra method to put the String from the EditText in the Intent
+        /*
+         * We use the putExtra method of the Intent class to pass some extra stuff to the
+         * Activity that we are starting. Generally, this data is quite simple, such as
+         * a String or a number. However, there are ways to pass more complex objects.
+         */
+        startChildActivityIntent.putExtra(Intent.EXTRA_TEXT, textEntered)
+
         startActivity(startChildActivityIntent)
     }
 }
